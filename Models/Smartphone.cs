7531d60f183105c6d1dfcea07712e9c947bac874
João Batista <joaobmds@outlook.com>
2023-12-17 02:00:45 -0300

    Implementando os atributos e metodos

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,19 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+        protected string Modelo { get ; set; }
+        protected string Imei { get ; set; }
+        protected int Memoria { get ; set; }
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // FEITO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string imei, string modelo,int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Memoria = memoria;
+            Modelo = modelo;
+            Imei = imei;
+            // FEITO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nk1 = new Nokia("12345","11111","nk1",64);
+nk1.Ligar();
+nk1.InstalarAplicativo("tiktok");
+Iphone iphone7 = new Iphone("12345","1111","iphone7",64);
+iphone7.Ligar();
+iphone7.InstalarAplicativo("Whatsapp2");
